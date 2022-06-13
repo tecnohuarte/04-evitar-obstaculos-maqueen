b889a0171471b0e7ae81429deb6d8a1ab6fc04cf
tecnohuarte <106558201+tecnohuarte@users.noreply.github.com>
2022-06-13 21:53:40 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+let Izquierda = false
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 30) {
+        Izquierda = Math.randomBoolean()
+        if (Izquierda == true) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            basic.pause(600)
+        } else {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            basic.pause(600)
+        }
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    }
+})
